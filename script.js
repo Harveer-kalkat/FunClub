632da3afbc554637b64b9970139c58321f970987
Harveer-kalkat <harrykalkat@gmail.com>
2023-09-23 11:00:10 -0700

    Form created

---
+++ script.js
@@ -0,0 +1,14 @@
+const players = [
+  {
+    Name: "John",
+    Email: "john@fake.com",
+    Sport: "Soccer",
+    Location: "Vancouver",
+  },
+  {
+    Name: "Cindy",
+    Email: "Cindy@fake.com",
+    Sport: "Tennis",
+    Location: "Burnaby",
+  },
+];
